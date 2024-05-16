2ed196e282086f1cf6930430725408b8f26958a6
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2024-05-15 22:13:56 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Papa from 'papaparse';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import fs from 'fs';
 import path from 'path';
+import styles from '../styles/Home.module.css';
 
 export default function Home({ data }) {
   const [parsedData, setParsedData] = useState([]);
@@ -16,23 +17,25 @@ export default function Home({ data }) {
   }, [data]);
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Gastos en Campañas</h1>
-      <BarChart
-        width={600}
-        height={300}
-        data={parsedData}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="Page name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="Amount spent (MXN)" fill="#8884d8" />
-        <Bar dataKey="Number of ads in Library" fill="#82ca9d" />
-      </BarChart>
+      <div className={styles.chart}>
+        <BarChart
+          width={600}
+          height={300}
+          data={parsedData}
+          margin={{
+            top: 5, right: 30, left: 20, bottom: 5,
+          }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="Page name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Amount spent (MXN)" fill="#8884d8" />
+          <Bar dataKey="Number of ads in Library" fill="#82ca9d" />
+        </BarChart>
+      </div>
     </div>
   );
 }
