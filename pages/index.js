d8b1b6c68b7f280358fea2dd540ddb38c8b68e53
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2024-05-16 22:08:04 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ import styles from '../styles/Home.module.css';
 
 const COLORS = ['#8884d8', '#82ca9d', '#ffc658'];
 
+const formatNumber = (num) => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
 export default function Home({ data }) {
   const [parsedData, setParsedData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
@@ -42,7 +46,7 @@ export default function Home({ data }) {
       <Typography variant="h2" component="h1" gutterBottom className={styles.projectName}>
         B.E.N.I.T.O
       </Typography>
-      <Typography variant="h6" component="p" gutterBottom className={styles.projectDescription}>
+      <Typography variant="h6" component="p" gutterBottom className={styles.projectDescription} style={{ fontSize: '14px' }}> {/* Hacer el texto más pequeño */}
         Búsqueda En Networks Inteligentes para Transparencia Oficial
       </Typography>
       <Typography variant="h3" component="h2" gutterBottom className={styles.heading}>
@@ -54,14 +58,14 @@ export default function Home({ data }) {
             <BarChart
               data={filteredData}
               margin={{
-                top: 60, right: 30, left: 20, bottom: 60,
+                top: 80, right: 30, left: 20, bottom: 60, // Ajustar el margen superior para más espacio
               }}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="Page name" angle={-25} textAnchor="end" interval={0} />
-              <YAxis yAxisId="left" />
-              <YAxis yAxisId="right" orientation="right" />
-              <Tooltip />
-              <Legend verticalAlign="top" height={36} />
+              <YAxis yAxisId="left" tickFormatter={formatNumber} />
+              <YAxis yAxisId="right" orientation="right" tickFormatter={formatNumber} />
+              <Tooltip formatter={formatNumber} />
+              <Legend verticalAlign="top" wrapperStyle={{ top: 0 }} /> {/* Mover la leyenda hacia arriba */}
               <Bar yAxisId="left" dataKey="Amount spent (MXN)" fill="#8884d8" barSize={20} />
               <Bar yAxisId="right" dataKey="Number of ads in Library" fill="#82ca9d" barSize={20} />
             </BarChart>
@@ -84,15 +88,15 @@ export default function Home({ data }) {
                   <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
               </Pie>
-              <Tooltip />
+              <Tooltip formatter={formatNumber} />
             </PieChart>
           </ResponsiveContainer>
         </Box>
       </Box>
       <footer className={styles.footer}>
-        Datos obtenidos de <a href="https://www.facebook.com/ads/library/report/" target="_blank" rel="noopener noreferrer">Facebook Network (META)</a>.
+        Datos obtenidos de <a href="https://www.facebook.com/ads/library/report/" target="_blank" rel="noopener noreferrer" style={{ color: '#1e90ff' }}>Facebook Network (META)</a>.
         <br />
-        Made with ❤️ by <a href="https://www.linkedin.com/in/jonathanftw/" target="_blank" rel="noopener noreferrer">Jonathan Paz</a>
+        Made with ❤️ by <a href="https://www.linkedin.com/in/jonathanftw/" target="_blank" rel="noopener noreferrer" style={{ color: '#1e90ff' }}>Jonathan Paz</a>
       </footer>
     </Container>
   );
